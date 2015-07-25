5f305dc953d8916f74844ccd05ad98daed2150d6
rajathkumarmp <rajathkumarmp1995@gmail.com>
2015-07-25 23:07:34 +0530

    edit 2

--- Tutorial-2/bin/startDebugsecondcomp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./secondcomp --Ice.Config=../etc/generic_config.debug,../etc/specific_config.debug &
--- Tutorial-2/bin/startsecondcomp.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./secondcomp --Ice.Config=../etc/generic_config,../etc/specific_config &
